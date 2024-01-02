f24185abc95670ff951006c1454b4a0ca978ad66
Ghelle8 <154821055+Ghelle8@users.noreply.github.com>
2024-01-02 15:56:56 +0000

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,18 +1,22 @@
 import random 
+
 # A list of 5 of my favourite fruits
 fruit_list = ["Apple", "Banana", "Mango", "Strawberry", "Pineapple"]
+
 # Generate a random choice of fruit from the list of fruits above
 random_fruit = random.choice(fruit_list)
+
 # Ask the user to enter a single letter
 guess_first_letter = input("Please enter a single letter: ")
+
 # Check if the input is a single letter and alphabetical
-for guess in guess_first_letter:
-    if len(guess) == 1 and guess.isalpha():
-    print("Good Guess!:", guess)
+if len(guess_first_letter) == 1 and guess_first_letter.isalpha():
+    print("Good Guess!:", guess_first_letter)
 else:
-    print("Oops! That is not a vaild input.")
+    print("Oops! That is not a valid input.")
 
 # Display the entered letter
-print("You entered:", guess)
+print("You entered:", guess_first_letter)
+
 # Display randomly selected fruit
 print("Randomly selected fruit:", random_fruit)
